554064038faba7831c74c4c025faec4a79eaf7df
alvaroLeitao <aleitao85@gmail.com>
2017-04-04 11:55:13 +0200

    Compiling file.

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@
 
 ulimit -s unlimited
 
-# gcc -L$GSL_ROOT/lib -I$GSL_ROOT/include SGBM_ArithBasket.c -lgsl -lgslcblas -lm -O3
-gcc SGBM_ArithBasket.c -lgsl -lgslcblas -lm -O3
+gcc -L$GSL_ROOT/lib -I$GSL_ROOT/include SGBM_ArithBasket.c -lgsl -lgslcblas -lm -O3
+# gcc SGBM_ArithBasket.c -lgsl -lgslcblas -lm -O3
 
 #for((i=0;i<30;i++))
 #do
